54dfbe61b4dc8d85d419b16a6e0db3248d7a00c7
Asker <kotsevasker0@gmail.com>
2024-05-14 13:48:57 +0300

    create file for learn js

---
+++ array.js
@@ -0,0 +1,54 @@
+//forEach
+{
+    let arr = [1,2,3,4,5,6,7]
+}
+//map
+{
+    let arr = [1,2,3,4,5,6,7]
+}
+//reduce
+{
+    let arr = [1,2,3,4,5,6,7]
+}
+//slice
+{
+    let arr = [1,2,3,4,5,6,7]
+}
+//splice
+{
+    let arr = [1,2,3,4,5,6,7]
+}
+//for of
+{
+    let arr = [1,2,3,4,5,6,7]
+}
+//find
+{
+    let arr = [
+        {userName:"Asker",age:26},
+        {userName:"Hloe",age:27}
+    ]
+}
+//findIndex
+{
+    let arr = [
+        {userName:"Asker",age:26},
+        {userName:"Hloe",age:27}
+    ]
+}
+//destructure
+{
+    let arr = [
+        {userName:"Asker",age:26},
+        {userName:"Hloe",age:27}
+    ]
+}
+//reverce
+{
+    let arr = [
+        {userName:"Asker",age:26},
+        {userName:"Hloe",age:27}
+    ]
+}
+
+

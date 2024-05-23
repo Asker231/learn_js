56b175eabe7c6efdb09363578acd2fb25e5c84e5
Asker <kotsevasker0@gmail.com>
2024-05-23 22:23:02 +0300

    Objects

--- object.js
+++ object.js
@@ -5,7 +5,7 @@
 //Не явное связывание
 {
     function Some(){
-        console.log(this.a);
+       // console.log(this.a);
     }
 
     const obj = {
@@ -17,7 +17,7 @@
 //явное связывание
 {
     function Some(){
-        console.log(this.a);
+       // console.log(this.a);
     }
 
     const obj = {
@@ -28,7 +28,7 @@
 //жесткое связывание
 {
     function Some(el){
-        console.log(this.a + el);
+       // console.log(this.a + el);
     }
 
     const obj = {
@@ -43,10 +43,55 @@
 {
     function Some(a){
         this.a = a
-        console.log(this.a);
+       // console.log(this.a);
     }
 
     let some = new Some(10)
     //1) Создается новый объект 
     //2) This связывается с созданным объектом
+}
+
+//Object
+{
+  //поверхностное копирование объектов
+  const obj = {
+    value:10
+  }
+  const newObj1 = Object.create(obj)//этот метод копирует объект но при вызове в логи там пусто 
+  // а если обратиться к свойствам они выводятся как внизу
+  console.log(newObj1.value);//vlaue = 10
+
+  const someObj = {
+    newValue:11
+  }
+  const newObj2 = Object.assign({},obj,someObj)
+  //Этот метод принимает в качестве первого аргумента , объект получатель, и несколько объектов которые 
+  //нужно объеденить в один
+  // но если у объектов одинаковые свойства в исходный объект попадет только последний
+  console.log(newObj2);//{ value: 10, newValue: 11 }
+
+
+
+
+
+  
+  //глубокое копирование
+  //посредством spred или ... мы так же можем копировать объекты и даже вложенные
+  const spredObj = {
+    ...obj,
+    ...someObj
+  }
+  console.log(spredObj);//{ value: 10, newValue: 11 }
+
+  //так же есть и другой способ но он не такой хороший
+  //данный метод тоже копирует объект типо глубоко
+  // НО если в объекте есть методы или даты он с ними не работает
+  const jsonObj = JSON.parse(JSON.stringify(spredObj))
+  console.log(jsonObj);//{ value: 10, newValue: 11 }
+
+  //и самый последний способ structuredClone этот метод вроде как копирует объект глубоко
+  const structObj = structuredClone(jsonObj)
+  console.log(structObj);//{ value: 10, newValue: 11 }
+
+
 }
